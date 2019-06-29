222b26a1f1f0915f836a0cd8c180c2426b5a2f0a
jdandois <jon.dandois@gmail.com>
2019-06-28 21:37:57 -0400

    adding in mongoose and models

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +9,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+// setup Mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGODB;
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
